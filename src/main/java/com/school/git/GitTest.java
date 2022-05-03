889b9c27593343a3fa7b86fedecf7c3ee927ff0a
CN-Marvin <96917978+CN-Marvin@users.noreply.github.com>
2022-05-03 23:42:06 +0800

    Update GitTest.java

--- src/main/java/com/school/git/GitTest.java
+++ src/main/java/com/school/git/GitTest.java
@@ -10,9 +10,6 @@ package com.school.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git1");
-        System.out.println("git2");
-        System.out.println("git3");
-        System.out.println("git4");
         System.out.println("master");
         System.out.println("hotfix");
     }
