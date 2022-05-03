9667e148a5811a7e04977f051dcdcc5423f19bb8
marvin <865462841@qq.com>
2022-05-03 23:17:49 +0800

    fist commit

---
+++ src/main/java/com/school/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.school.git;
+
+/**
+ * 功能描述：
+ *
+ * @Package: com.school.git
+ * @author: msi-pc
+ * @date: 2022/5/3 23:14
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+}
---
+++ src/main/java/com/school/git/GitTest1.java
@@ -0,0 +1,14 @@
+package com.school.git;
+
+/**
+ * 功能描述：
+ *
+ * @Package: com.school.git
+ * @author: msi-pc
+ * @date: 2022/5/3 23:15
+ */
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("git2");
+    }
+}
